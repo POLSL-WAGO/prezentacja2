2610c0a14880eedd1d055cb4ee9bd3f3ae37be32
wisnia97 <1b13wisniewski@gmail.com>
2020-06-12 20:05:26 +0200

    Add view to algorithm1

--- drawlib.js
+++ drawlib.js
@@ -16,7 +16,7 @@ function DrawPoint(pkt_x, pkt_y, pkt_color) {
   // funkcja rysująca punkt
   console.log(pkt_x + "," + pkt_y + "," + pkt_color);
   ctx.beginPath();
-  ctx.arc(pkt_x, pkt_y, 6, 0, 2 * Math.PI, false);
+  ctx.arc(pkt_x, pkt_y, 2, 0, 2 * Math.PI, false);
   //console.log("x= " + pkt_x + " y= " + pkt_y); w konsoli wyświtla współrzene punktu
   ctx.fillStyle = pkt_color;
   ctx.fill();
@@ -55,4 +55,103 @@ function DrawPolygon(Points2) {
   ctx.closePath();
   ctx.strokeStyle = pol_color;
   ctx.stroke();
+}
+
+var points = [];
+var kwadrat = {
+  x: 12,
+  y: 10,
+  w: 100,
+  h: 50,
+};
+
+function DrawRectangle(rec_x, rec_y, rec_h, rec_w, rec_color){
+  console.log(rec_x, rec_y, rec_h, rec_w, rec_color);
+  ctx.strokeStyle = rec_color;
+  ctx.strokeRect(rec_x,  rec_y, rec_w, rec_h);
+  rectangle_points(kwadrat, points);
+}
+
+function randomPoints(pktRandom){
+    console.log(points);
+    generateArray(pktRandom, 500);
+    for(i = 0; i < pktRandom; i++){
+        DrawPoint(points[i].x, points[i].y, 0);
+    }
+  }
+
+  function randomLines(linRandom){
+    console.log(linRandom);
+    var x1 = 0;
+    var y1 = 0;
+    var x2 = 0;
+    var y2
+    for(i = 0; i < linRandom; i++){
+        x1 = getRandomInt(0, 500);
+        y1 = getRandomInt(0, 500);
+        x2 = getRandomInt(0, 500);
+        y2 = getRandomInt(0, 500);
+        DrawLine(x1, y1, x2, y2, 0)
+    }
+  }
+
+  function randomDirectionLine(lin_x, lin_y, linRandom){
+    console.log(linRandom);
+    console.log(lin_x);
+    console.log(lin_y);
+    var x2 = 0;
+    var y2
+    for(i = 0; i < linRandom; i++){
+        x2 = getRandomInt(0, 500);
+        y2 = getRandomInt(0, 500);
+        DrawLine(lin_x, lin_y, x2, y2, 0)
+    }
+  }
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateArray(size, range) {
+  //generuje tablice punktów w argumencie przyjmuje wielkość tablicy
+  for (let index = 0; index < size; index++) {
+    points.push({ x: getRandomIntInclusive(0, range), y: getRandomIntInclusive(0, range) }); //przedział w którym generowane sąpunkty można zmienić
+  }
+}
+
+// Algorytm1
+//zliczanie punktów leżących w prostokącie
+function rectangle_points(rect, points) {
+  // argumenty funkcji => rect obiekt zawiera wysokość szerokosć i punkt środkowy, points to tablica z obiektami punktów zawiera wartość x i y
+  var counter = 0;
+  console.log(points);
+  console.log(rect);
+  points.forEach((el) => {
+    if (rect.w == 0 && rect.h == 0) {
+      counter++;
+    } else if (rect.w > 0 && rect.h > 0) {
+      if (rect.x < el.x && el.x < rect.x + rect.w && rect.y < el.y && el.y < rect.y + rect.h) counter++;
+    } else if (rect.w < 0 && rect.h > 0) {
+      if (rect.x + rect.w < el.x && el.x < rect.x && rect.y < el.y && el.y < rect.y + rect.h) counter++;
+    } else if (rect.w > 0 && rect.h < 0) {
+      if (rect.x < el.x && el.x < rect.x + rect.w && rect.y + rect.h < el.y && el.y < rect.y) counter++;
+    } else if (rect.w < 0 && rect.h < 0) {
+      if (rect.x + rect.w < el.x && el.x < rect.x && rect.y + rect.h < el.y && el.y < rect.y) counter++;
+    }
+  });
+  document.getElementById("result").innerHTML = "Liczba punktów w prostokącie: " + counter;
+  console.log("Liczba punktów w prostokącie" + counter);
+}
+
+
+function rectangleCheck(rec_x, rec_y, rec_h, rec_w, rec_color, pkt_size){
+  kwadrat.x = rec_x;
+  kwadrat.y = rec_y;
+  kwadrat.h = rec_h;
+  kwadrat.w = rec_w;
+  DrawRectangle(rec_x, rec_y, rec_h, rec_w, rec_color);
+  randomPoints(pkt_size);
+  rectangle_points(kwadrat, points);
 }
