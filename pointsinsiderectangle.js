67db868bc5290ec39499c9208efb71aa188f4a33
Marcin Rojek <marcin.rojek@outlook.com>
2020-06-12 18:03:22 +0200

    reactangle points algorithm fixed

--- pointsinsiderectangle.js
+++ pointsinsiderectangle.js
@@ -1,3 +1,16 @@
+// constructor(center, radius, color){
+//this.center = center;
+//this.radius = radius;
+//this.color = color;
+//this.type = "circle";
+//  }
+
+//var Point = {
+
+//x: 12
+// y:10
+
+//};
 //zliczanie punktów leżących w prostokącie
 function rectangle_points(rect, points) {
   // argumenty funkcji => rect obiekt zawiera wysokość szerokosć i punkt środkowy, points to tablica z obiektami punktów zawiera wartość x i y
@@ -7,37 +20,13 @@ function rectangle_points(rect, points) {
     if (rect.w == 0 && rect.h == 0) {
       counter++;
     } else if (rect.w > 0 && rect.h > 0) {
-      if (
-        rect.x < el.center.x &&
-        el.center.x < rect.x + rect.w &&
-        rect.y < el.center.y &&
-        el.center.y < rect.y + rect.h
-      )
-        counter++;
+      if (rect.x < el.x && el.x < rect.x + rect.w && rect.y < el.y && el.y < rect.y + rect.h) counter++;
     } else if (rect.w < 0 && rect.h > 0) {
-      if (
-        rect.x + rect.w < el.center.x &&
-        el.center.x < rect.x &&
-        rect.y < el.center.y &&
-        el.center.y < rect.y + rect.h
-      )
-        counter++;
+      if (rect.x + rect.w < el.x && el.x < rect.x && rect.y < el.y && el.y < rect.y + rect.h) counter++;
     } else if (rect.w > 0 && rect.h < 0) {
-      if (
-        rect.x < el.center.x &&
-        el.center.x < rect.x + rect.w &&
-        rect.y + rect.h < el.center.y &&
-        el.center.y < rect.y
-      )
-        counter++;
+      if (rect.x < el.x && el.x < rect.x + rect.w && rect.y + rect.h < el.y && el.y < rect.y) counter++;
     } else if (rect.w < 0 && rect.h < 0) {
-      if (
-        rect.x + rect.w < el.center.x &&
-        el.center.x < rect.x &&
-        rect.y + rect.h < el.center.y &&
-        el.center.y < rect.y
-      )
-        counter++;
+      if (rect.x + rect.w < el.x && el.x < rect.x && rect.y + rect.h < el.y && el.y < rect.y) counter++;
     }
   });
 
